c40bdd10b8b0b5bbb89e3f1d613475a060169961
SZTerminator <55211863+SZTerminator@users.noreply.github.com>
2021-09-21 18:58:09 +0300

    angular update

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+     
+@Component({
+    selector: 'my-app',
+    templateUrl: `./templats/a.html`
+})
+export class AppComponent { 
+    name= '';
+}
+
---
+++ src/app/task.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:`task`,
+    templateUrl:`./templats/task.html`
+})
+export class TaskComponent{
+
+}
